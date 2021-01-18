70501f5a4c7bb06d7b92097edfe90f92519d443b
cindy <ciindy.wu@gmail.com>
2021-01-17 19:54:03 -1000

    Adds query to create, get, delete user including validations

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,14 +1,8 @@
 -- name: create-user!
 -- creates a new user record
 INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
-
--- name: update-user!
--- update an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
-WHERE id = :id
+(id, pass)
+VALUES (:id, :pass)
 
 -- name: get-user
 -- retrieve a user given the id.
