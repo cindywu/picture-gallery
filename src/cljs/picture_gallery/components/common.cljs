4ff3457a0b9a4e5a9da97b9046334d1c5ab59a36
cindy <ciindy.wu@gmail.com>
2021-01-17 22:11:00 -1000

    Add registration component

--- src/cljs/picture_gallery/components/common.cljs
+++ src/cljs/picture_gallery/components/common.cljs
@@ -2,6 +2,29 @@
   (:require [reagent.core :refer [atom]]
             [reagent.session :as session]))
 
+(defn input [type id placeholder fields]
+  [:input.form-control.input-lg
+   {:type type
+    :placeholder placeholder
+    :value (id @fields)
+    :on-change #(swap! fields assoc id (-> % .-target .-value))}])
+
+(defn form-input [type label id placeholder fields optional?]
+  [:div.form-group
+   [:label label]
+   (if optional?
+     [input type id placeholder fields]
+     [:div.input-group
+      [input type id placeholder fields]
+      [:span.input-group-addon
+       "*"]])])
+
+(defn text-input [label id placeholder fields & [optional?]]
+  (form-input :text label id placeholder fields optional?))
+
+(defn password-input [label id placeholder fields & [optional?]]
+  (form-input :password label id placeholder fields optional?))
+
 (defn modal [header body footer]
   [:div
    [:div.modal-dialog
---
+++ src/cljs/picture_gallery/components/registration.cljs
@@ -0,0 +1,18 @@
+(ns picture-gallery.components.registration
+  (:require [reagent.core :refer [atom]]
+            [picture-gallery.components.common :as c]))
+
+(defn registration-form []
+  (let [fields (atom {})]
+    (fn []
+      [c/modal
+       [:div "Picture Gallery Registration"]
+       [:div
+        [:div.well.well-sm
+         [:strong "* required field"]]
+        [c/text-input "name" :id "enter a username" fields]
+        [c/password-input "password" :pass "enter a password" fields]
+        [c/password-input "password" :pass-confirm "re-enter the password" fields]]
+       [:div
+        [:button.btn.btn-primary "Register"]
+        [:button.btn.btn-danger "Cancel"]]])))
